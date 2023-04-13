cc224ad6c03d4d272bf9f8f7a2d8077f16015cad
AkashPrajapati2304 <202051013@iiitvadodara.ac.in>
2023-04-13 18:46:23 +0530

    build file change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: "/node_modules/.pnpm/rollup@3.20.2",
+    },
+  },
+});
